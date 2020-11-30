4121b0881b83b29dae8e8d97b9a08dbcb2c46915
user <user@Ricky>
2020-11-30 13:17:34 -0600

    Mensaje en ingles

---
+++ mensaje en ingles.c
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   mensaje en ingles.c
+ * Author: user
+ *
+ * Created on 30 de noviembre de 2020, 01:11 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    printf("Hi Ricky");
+    return (EXIT_SUCCESS);
+}
+
